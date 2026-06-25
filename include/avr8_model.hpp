4dcc50f2955aa924ad2f99f7c738ff2c1a2eaf49
AlexRosito67 <rosito007@hotmail.com>
2026-06-25 14:42:51 -0700

    Uploaded .gitignore

--- include/avr8_model.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "model_base.hpp"
-
-class Avr8Model : public ModelBase {
-public:
-    UartResult compute(double clock, double baud) override;
-};
--- include/model_base.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "uart_result.hpp"
-
-class ModelBase {
-public:
-    virtual UartResult compute(double clock, double baud) = 0;
-    virtual ~ModelBase() = default;
-};
